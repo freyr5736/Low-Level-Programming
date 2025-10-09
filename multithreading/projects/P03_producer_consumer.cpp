e208e929930cfdb3289d32042860d6a2fffe138f
freyr5736 <bhaskark.kuniyal@gmail.com>
2025-10-09 11:31:03 +0000

    threadpool on producer n consumer

--- multithreading/projects/P03_producer_consumer.cpp
+++ multithreading/projects/P03_producer_consumer.cpp
@@ -1,3 +1,4 @@
+#include "thread_pool.hpp"
 #include <algorithm>
 #include <atomic>
 #include <chrono>
@@ -16,7 +17,7 @@ std::condition_variable cv;
 
 void producer(int task) {
     while (task) {
-        std::unique_lock<std::mutex> lock (m);
+        std::unique_lock<std::mutex> lock(m);
         task_queue.push_back(task);
         // std::osyncstream out(std::cout);
         // out << "\r" << task << " in queue"<<std::flush;
@@ -32,7 +33,7 @@ void consumer() {
         std::unique_lock<std::mutex> lock(m);
         cv.wait(lock, []() { return !task_queue.empty(); });
         std::osyncstream out(std::cout);
-        out << "\r" << task_queue.front() << " completed"<<std::flush;
+        out << "\r" << task_queue.front() << " completed" << std::flush;
         task_queue.pop_front();
         lock.unlock();
         cv.notify_all();
@@ -45,6 +46,7 @@ void consumer() {
 int main() {
     std::vector<std::thread> all;
     all.reserve(std::thread::hardware_concurrency());
+    // Level #1 Basic Call
     auto start_time = std::chrono::high_resolution_clock::now();
     for (int i = 1; i <= 4; ++i) {
         if (i % 2 == 0) {
@@ -58,7 +60,22 @@ int main() {
         t.join();
     }
     auto stop_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop_time - start_time);
-    std::cout<< "\n All Tasks Completed in "<<duration.count()<<"μs\n";
+    auto duration =
+    std::chrono::duration_cast<std::chrono::microseconds>(stop_time -start_time); 
+    std::cout<< "\nTime Elapsed "<<duration.count()<<" μs\n";
+
+    // Level #2 Thread pool
+    // auto start_time = std::chrono::high_resolution_clock::now();
+    // thread_pool producer_pool(2);
+    // auto a = producer_pool.enqueue([&]() { producer(10000000); });
+    // thread_pool consumer_pool(2);
+    // auto b = consumer_pool.enqueue([&]() { consumer(); });
+
+    // a.get();
+    // b.get();
+
+    // auto stop_time = std::chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop_time - start_time);
+    // std::cout << "\n Time Elapsed " << duration.count() << " μs\n";
     return 0;
 }
